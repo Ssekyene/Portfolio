b02a2f0cd317cc7344a31be0404d2c8fb15fa7f6
Ssekyene Robert <robertssekyene05@gmail.com>
2024-12-18 15:46:09 +0300

    made the home link become active on clicking the logo

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-// toggle for the navbar
+/****** toggle for the navbar  *****/
 const menuIcon = document.querySelector('.menu-icon');
 const navItems = document.querySelector('.nav-items');
 const ctn = document.querySelector('header .container');
@@ -15,7 +15,7 @@ menuIcon.addEventListener('click', () => {
 
 });
 
-// apply styles for active nav-link
+/****** apply styles for active nav-link ******/
 const links =  document.querySelectorAll('.nav-link');
 links.forEach(link => {
   link.addEventListener('click', function() {
@@ -26,21 +26,56 @@ links.forEach(link => {
   });  
 });
 
+
 function setActiveLink() {
   // remove active class from all links and add it only on the active link
   links.forEach(link => link.classList.remove('active'));
 
   const activeLink = localStorage.getItem('activeLink');
   if (activeLink) {
-    const link = document.querySelector(`a[href="${activeLink}"]`);
-    if (link) {
-      link.classList.add('active');
+    const activeAnchor = document.querySelector(`a[href="${activeLink}"]`);
+    if (activeAnchor) {
+      activeAnchor.classList.add('active');
     }
   }
 }
 
 // call on page reload
 setActiveLink();
+
+
+/****** apply styles to activate home nav-link when logo is clicked *****/
+const logo = document.querySelector('#logo');
+const indexLink = document.querySelector('a[href="./index.html"]');
+const navLinks = document.querySelectorAll('.nav-link');
+
+// Set the index link active when the logo is clicked
+function setActiveIndexLink(lnk){
+  navLinks.forEach(link => link.classList.remove('active'));
+  lnk.classList.add('active');
+  localStorage.setItem('indexLink', lnk.getAttribute('href')); // storing the active link
+}
+
+logo.addEventListener('click', function() {
+  localStorage.setItem('logoClicked', 'true');
+  setActiveIndexLink(indexLink);
+});
+
+// activating the index page nav link on page reload when the logo is clicked
+const logoClicked = localStorage.getItem('logoClicked');
+if (logoClicked === 'true') {
+  document.addEventListener('DOMContentLoaded', function() {
+      localStorage.removeItem('logoClicked');
+      const indexLinkPath = localStorage.getItem('indexLink');
+      if (indexLinkPath) {
+        const indexLink = document.querySelector(`a[href="${indexLinkPath}"]`);
+      }
+      if (indexLink) {
+        setActiveIndexLink(indexLink);
+      }
+  });
+}
+
 
 // update the container's height back to normal and displays content for large screens
 function updateCtn() {
